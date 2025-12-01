521094d016d12fe47d18f8b9bfd018abd116894a
s5830360 <s5838369@gmail.com>
2025-12-01 13:44:56 +0900

    Remove spring boot project

--- capstone_design/src/main/java/com/example/capstone_design/repository/RecordingRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.capstone_design.repository;
-
-import com.example.capstone_design.entity.Recording;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RecordingRepository extends JpaRepository<Recording, Long>{
-}
